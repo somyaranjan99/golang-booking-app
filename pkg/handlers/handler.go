f820ef2cb1c4cbdbc03a5b4987ae155e00b35e18
somyaranjan99 <bapuni99@gmail.com>
2025-03-29 19:29:59 +0530

    added summary page and validation

--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"fmt"
+	"github/somyaranjan99/basic-go-project/cmd/web/middleware/forms"
 	"github/somyaranjan99/basic-go-project/pkg/config"
 	"github/somyaranjan99/basic-go-project/pkg/models"
 	"github/somyaranjan99/basic-go-project/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -43,10 +45,7 @@ func (repo *Repository) Majors(w http.ResponseWriter, r *http.Request) {
 
 	render.RenderTemplate(w, r, "majors.page.tmpl", repo.Repo, &models.TemplateData{})
 }
-func (repo *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", repo.Repo, &models.TemplateData{})
-}
 func (repo *Repository) Aavailability(w http.ResponseWriter, r *http.Request) {
 
 	render.RenderTemplate(w, r, "search-availability.page.tmpl", repo.Repo, &models.TemplateData{})
@@ -60,4 +59,66 @@ func (repo *Repository) PostAavailability(w http.ResponseWriter, r *http.Request
 	start := r.Form.Get("start")
 	end := r.Form.Get("end")
 	w.Write([]byte(fmt.Sprintf("start date is %s end date is %s", start, end)))
+}
+
+func (repo *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", repo.Repo, &models.TemplateData{
+		Form: forms.New(nil),
+		Data: data,
+	})
+}
+
+func (repo *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	reservation := &models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Email:     r.Form.Get("email"),
+		Phone:     r.Form.Get("phone"),
+	}
+	form := forms.New(&r.PostForm)
+	form.Has("first_name", r)
+	form.Has("last_name", r)
+	form.Has("email", r)
+	form.Has("phone", r)
+	// form.Required("first_name", "last_name", "email", "phone")
+	form.MinLength("first_name", 3, r)
+	form.MinLength("last_name", 3, r)
+	form.IsValidEmail("email")
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+		render.RenderTemplate(w, r, "make-reservation.page.tmpl", repo.Repo, &models.TemplateData{
+			Data: data,
+			Form: form,
+		})
+		return
+	}
+	repo.Repo.Session.Put(r.Context(), "reservation", reservation)
+	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
+}
+func (repo *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
+	reservation, ok := repo.Repo.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		log.Println("can't get item from session")
+		repo.Repo.Session.Put(r.Context(), "error", "Can't get reservation from session")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	repo.Repo.Session.Remove(r.Context(), "reservation")
+
+	data := make(map[string]interface{})
+	data["reservation"] = reservation
+	render.RenderTemplate(w, r, "reservation-summary.page.tmpl", repo.Repo, &models.TemplateData{
+		Data: data,
+	})
+
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -12,6 +12,9 @@ import (
 func RenderTemplate(w http.ResponseWriter, r *http.Request, templ string, app *config.AppConfig, m *models.TemplateData) {
 	csrfToken := nosurf.Token(r)
 	m.CSRFToken = csrfToken
+	m.Error = app.Session.PopString(r.Context(), "error")
+	m.Flash = app.Session.PopString(r.Context(), "flash")
+	m.Warning = app.Session.PopString(r.Context(), "warning")
 	renderPage, err := template.ParseFiles("../../template/"+templ, "../../template/base.layout.tmpl")
 	if err != nil {
 		return
